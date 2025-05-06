2a01fa5b8fa69ffb21757ed8bc019cc941a6de3a
merlinkraemer <merlinkraemer@gmail.com>
2025-05-06 16:23:25 +0200

    added art wip gallery with js lightbox and cloudflare r2 storage

---
+++ script.js
@@ -0,0 +1,17 @@
+function openLightbox(img) {
+    const lightbox = document.getElementById('lightbox');
+    const lightboxImg = document.getElementById('lightbox-img');
+    lightboxImg.src = img.src;
+    lightbox.style.display = 'block';
+}
+
+function closeLightbox() {
+    const lightbox = document.getElementById('lightbox');
+    lightbox.style.display = 'none';
+}
+
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'Escape') {
+        closeLightbox();
+    }
+}); 
